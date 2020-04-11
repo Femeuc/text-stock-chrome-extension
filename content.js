91c2ac029c93bc70c64bf7110e12a1331073e6be
Felipe Mesquita Uchoa <femeuf5@gmail.com>
2020-04-11 12:38:34 -0300

    Deletes useless content script

--- content.js
+++
@@ -1,6 +0,0 @@
-console.log("Chrome extension ready to go!!!");
-
-chrome.storage.onChanged.addListener(function(changes, namespace) {
-    let hai = chrome.storage.sync.get(['key']);
-console.log(hai);
-});
